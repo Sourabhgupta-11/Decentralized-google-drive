d667ea002a070ad8189972628e3357671b30a882
Sourabh Gupta <sourabhgupta.1331@gmail.com>
2026-02-08 03:31:44 +0530

    create home page

--- frontend/src/contract.js
+++ frontend/src/contract.js
@@ -1,9 +1,11 @@
 import { ethers } from "ethers";
-import ABI from "./DecentralizedDrive.json";
+import Abi from "./DecentralizedDrive.json";
 export const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
 
 export const getContract = async () => {
   const provider = new ethers.BrowserProvider(window.ethereum);
   const signer = await provider.getSigner();
+  const ABI=Abi.abi
+  
   return new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
 };
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Upload from "../components/Upload";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Home
+  return (
+    <div className="page">
+      <h1>Decentralized Drive</h1>
+
+      {/* Upload Section */}
+      <Upload />
+
+      <hr />
+
+      {/* Navigation Buttons */}
+      <button onClick={() => navigate("/my-files")}>
+        Get My Files
+      </button>
+
+      <button onClick={() => navigate("/shared-files")}>
+        Get Shared Files
+      </button>
+    </div>
+  );
+};
+
+export default Home;
