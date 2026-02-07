bf78f60bf94ea0a2b67eefd558b1522d0024eedc
Sourabh Gupta <sourabhgupta.1331@gmail.com>
2026-02-08 04:53:26 +0530

    Add removeShareAccess function in contract

--- contract/contracts/fileShare.sol
+++ contract/contracts/fileShare.sol
@@ -17,12 +17,12 @@ contract DecentralizedDrive {
     uint256 private fileCounter;  //total files in that contract
 
     mapping(uint256 => File) private files;  //eg: file[1]={1,name,ipfs,..} means in id 1 this file is there
-
     mapping(address => uint256[]) private userFiles; //eg: userFiles[0xabc...]=[1,3,6] this address has access to file id - 1,3,6
-
     mapping(address => uint256[]) private sharedFiles;  //eg: sharedFiles[0xabc..]=[2,4] this address has been shared with file id 2,4 and now access to it
-
     mapping(uint256 => mapping(address => bool)) private fileAccess;   //eg: fileAccess[1][0xabc..]=true means fileid 1 can be accessed by address 0xabc..
+    mapping(uint256 => mapping(address => address)) private sharedBy;
+    mapping(uint256 => address[]) private fileSharedWith;
+
 
     // ---------------- EVENTS ----------------(events are like notification)
     event FileUploaded(
@@ -41,6 +41,7 @@ contract DecentralizedDrive {
         uint256 indexed fileId,
         address indexed owner
     );
+
 
     // ---------------- UPLOAD FILE ----------------
     function uploadFile(
@@ -93,45 +94,36 @@ contract DecentralizedDrive {
 
     // ---------------- SHARE FILE ----------------
     function shareFile(uint256 _fileId, address _to) external {
-        require(fileAccess[_fileId][msg.sender], "No access to file");
+        require(fileAccess[_fileId][msg.sender], "No access");
         require(_to != address(0), "Invalid address");
 
-        // If sender is not owner, resharing must be allowed
         if (msg.sender != files[_fileId].owner) {
             require(files[_fileId].allowReshare, "Resharing not allowed");
         }
 
-        // Grant access - below code run only when owner of file do allowReshare is true
+        require(!fileAccess[_fileId][_to], "Already has access");
+
         fileAccess[_fileId][_to] = true;
         sharedFiles[_to].push(_fileId);
-
-        emit FileShared(_fileId, msg.sender, _to);
+        fileSharedWith[_fileId].push(_to);
+        sharedBy[_fileId][_to] = msg.sender;   //"to" address ko file kisne share ki thi (jisne ye function call kiya tha)
     }
 
-    // ---------------- GET FILE (OWN OR SHARED) ----------------
-    function getFile(uint256 _fileId) external view returns (File memory) {
-        require(fileAccess[_fileId][msg.sender], "Access denied");
-        return files[_fileId];
-    }
+    //--------------------REMOVE SHARE ACCESS---------------------------
+    function removeShareAccess(uint256 _fileId, address _user) external {
+        require(fileAccess[_fileId][_user], "User has no access");
 
-    // ---------------- CHECK ACCESS ----------------
-    function hasAccess(uint256 _fileId, address _user)
-        external
-        view
-        returns (bool)
-    {
-        return fileAccess[_fileId][_user];
-    }
+        address owner = files[_fileId].owner;
+        address sharer = sharedBy[_fileId][_user];
 
-    // ---------------- DELETE FILE ----------------
-    function deleteFile(uint256 _fileId) external {
-        require(files[_fileId].owner == msg.sender, "Not file owner");
+        require(
+            msg.sender == owner || msg.sender == sharer,
+            "Not authorized to remove access"
+        );
 
-        // Remove owner access
-        fileAccess[_fileId][msg.sender] = false;
+        fileAccess[_fileId][_user] = false;
 
-        // Remove from owner's file list
-        uint256[] storage ids = userFiles[msg.sender];
+        uint256[] storage ids = sharedFiles[_user];
         for (uint256 i = 0; i < ids.length; i++) {
             if (ids[i] == _fileId) {
                 ids[i] = ids[ids.length - 1];
@@ -140,9 +132,71 @@ contract DecentralizedDrive {
             }
         }
 
-        // Delete file metadata
-        delete files[_fileId];
+        address[] storage sharedList = fileSharedWith[_fileId];
+        for (uint256 i = 0; i < sharedList.length; i++) {
+            if (sharedList[i] == _user) {
+                sharedList[i] = sharedList[sharedList.length - 1];
+                sharedList.pop();
+                break;
+            }
+        }
 
+        delete sharedBy[_fileId][_user];
+    }
+
+
+
+
+    // ---------------- DELETE FILE ----------------
+    function deleteFile(uint256 _fileId) external {
+        require(files[_fileId].owner == msg.sender, "Not file owner");
+
+        fileAccess[_fileId][msg.sender] = false;
+
+        uint256[] storage ownerFiles = userFiles[msg.sender];
+        for (uint256 i = 0; i < ownerFiles.length; i++) {
+            if (ownerFiles[i] == _fileId) {
+                ownerFiles[i] = ownerFiles[ownerFiles.length - 1];
+                ownerFiles.pop();
+                break;
+            }
+        }
+
+        address[] storage sharedUsers = fileSharedWith[_fileId];
+        for (uint256 i = 0; i < sharedUsers.length; i++) {
+            address user = sharedUsers[i];
+
+            fileAccess[_fileId][user] = false;
+            uint256[] storage userShared = sharedFiles[user];
+            for (uint256 j = 0; j < userShared.length; j++) {
+                if (userShared[j] == _fileId) {
+                    userShared[j] = userShared[userShared.length - 1];
+                    userShared.pop();
+                    break;
+                }
+            }
+            delete sharedBy[_fileId][user];
+        }
+        delete fileSharedWith[_fileId];
+        delete files[_fileId];
         emit FileDeleted(_fileId, msg.sender);
+    }
+
+    //---------------------Additional Function----------------------
+    function getFile(uint256 _fileId) external view returns (File memory) {
+        require(fileAccess[_fileId][msg.sender], "Access denied");
+        return files[_fileId];
+    }
+
+    function hasAccess(uint256 _fileId, address _user)external view returns (bool){
+        return fileAccess[_fileId][_user];
+    }
+
+    function getFileSharedWith(uint256 _fileId) external view returns (address[] memory){
+        return fileSharedWith[_fileId];
+    }
+
+    function getSharedBy(uint256 _fileId, address _user)external view returns (address){
+        return sharedBy[_fileId][_user];
     }
 }
